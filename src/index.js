078cc5cfb337b5e232d35a1941e172c307b44308
Shim Soyoung <sovv12@gmail.com>
2018-11-05 15:21:01 +0900

    initial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import "@babel/polyfill"; // 이 라인을 지우지 말아주세요!
 
-document.querySelector('h1').addEventListener('click', async e => {
-  alert('Hello World!');
-});
+const templates = { // 이 객체를 계속 복사해서 쓸 것임
+  loginForm : document.querySelector('#login-form').content
+}
+const rootEl = document.querySelector('.root')
+
+function drawLoginForm(){
+
+  // 1. 템플릿 복사하기
+  const fragment = document.importNode(templates.loginForm, true) // 한 벌 복사 완료
+
+  // 2. 내용 채우고, 이벤트 리스너 등록하기
+  const loginFormEl = fragment.querySelector('.login-form')
+
+  loginFormEl.addEventListener('submit', async => { // 이벤트 리스너 안에서 통신을 할 예정이니 비동기 함수로 진행
+    alert('로그인 요청 전송')
+    e.preventDefault()
+  })
+
+  // 3. 문서 내부에 삽입하기
+  rootEl.appendChild(fragment)
+}
+
+drawLoginForm()
